716b1d1eaaf90b805ae10c79408292361960d633
iwindon <ivan.windon@icloud.com>
2024-09-04 20:42:39 -0400

    added parameters to the function get_token

--- openshift-api.py
+++ openshift-api.py
@@ -1,16 +1,24 @@
 import requests
 import os
 
-def get_token():
+def get_token(cluster, env, core):
     """Function to retrieve the token froim the OpenShift Cluster"""
-    cluster_url = "https://oauth-openshift.apps.openshift.example.com"
+    if core is True:
+        domain = "core.openshift.example.com"
+    else:
+        domain = "openshift.example.com"
+    cluster_url = f"https://oauth-openshift.apps.{cluster}-{env}.{domain}"
     username = os.environ["USER_NAME"]
     password = os.environ["PASSWORD"]
     cert_path = os.path.join(os.path.dirname(__file__), "cert.pem")
 
     response = requests.get(f"{cluster_url}/oauth/authorize?client_id=openshift-challenging-client&response_type=token",
-                           auth=(username, password), timeout=15, verify=cert_path, allow_redirects=True)
+                           auth=(username, password),
+                           timeout=15,
+                           verify=cert_path,
+                           allow_redirects=True
+                    )
     token = response.url.split("access_token=")[1].split("&")[0]
     return token
 
-token = get_token()
+openshift_token = get_token("dallas", "dev", False)
